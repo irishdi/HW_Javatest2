31f9bce8463e2285a3a508dabe9ddd3025ff7ad1
irishdi <irishdi@inbox.ru>
2021-11-09 12:50:56 +0300

    Lesson 5 to check

---
+++ src/Lesson_5/var2/AppData.java
@@ -0,0 +1,34 @@
+package Lesson_5.var2;
+
+import java.util.Arrays;
+
+public class AppData {
+    private String[] header;
+    private int[][] data;
+
+    public AppData(String[] header, int[][] data){
+        this.header= header;
+        this.data=data;
+    }
+
+    public String[] getHeader() {
+        return header;
+    }
+
+    public int[][] getData() {
+        return data;
+    }
+
+    public void setHeader(String[] header) {
+        this.header = header;
+    }
+
+    public void setData(int[][] data) {
+        this.data = data;
+    }
+    @Override
+    public String toString(){
+        return "AppData \n" + "header: " + Arrays.toString(header) + "\n" + "data: " + Arrays.deepToString(data);
+
+    }
+}
